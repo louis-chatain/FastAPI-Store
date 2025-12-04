96d616fb9b78b9c58ee722c7096f1dd3a47b1009
Louis Chatain <louischatain123@gmail.com>
2025-12-04 22:36:24 +0100

    added bg tasks and show the name of the product when calling endpoints read/read_all order

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import time
 from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.background import BackgroundTasks
 from redis_om import get_redis_connection, HashModel
 from dotenv import load_dotenv
 import requests
@@ -43,6 +45,7 @@ class ProductOrder(HashModel):
 
 class Order(HashModel):
     product_id: str
+    name: str
     price: float
     quantity: int
     fee: float
@@ -55,19 +58,24 @@ class Order(HashModel):
 
 
 @app.post("/orders")
-def create(productOrder: ProductOrder):
+def create(productOrder: ProductOrder, bg_tasks: BackgroundTasks):
     req = requests.get(f"http://localhost:8000/product/{productOrder.product_id}")
     product = req.json()
     the_fee = product["price"] * 0.2
     order = Order(
         product_id = productOrder.product_id,
+        name = product["name"],
         price = product["price"],
         quantity = productOrder.quantity,
         fee = the_fee,
-        total = (productOrder.quantity * product["price"]) + the_fee,
+        total = (productOrder.quantity * product["price"]) * 0.2,
         status = "pending",
     )
-    return order.save()
+    order.save()
+
+    bg_tasks.add_task(order_complete, order)
+
+    return order
 
 
 def format(pk: str):
@@ -75,6 +83,7 @@ def format(pk: str):
   return {
     'id': order.pk,
     'product_id': order.product_id,
+    'product_name': order.name,
     'fee': order.fee,
     'total': order.total,
     'quantity': order.quantity,
@@ -99,3 +108,17 @@ def read(pk: str):
             detail=f"Order pk {pk} not found.")
     return order
 
+
+@app.delete("/order/delete")
+def delete(pk: str):
+    order = Order.delete(pk)
+    if not order:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Order pk {pk} not found.")
+    return order
+
+
+def order_complete(order: Order):
+    time.sleep(10)
+    order.status = "Complete"
+    order.save()
